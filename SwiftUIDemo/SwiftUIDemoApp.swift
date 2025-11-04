c40084e47bfe226fd0ae234a527fb4c2cde60bc5
zhaijiang152 <qq1281167818@163.com>
2025-11-04 20:32:25 +0800

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by gaoang on 2025/11/4.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
